181b2e6ea0ac09d17b2f91f33c678ce28a1b03a4
oksanakleptikova@gmail.com <vladhoshko72@gmail.com>
2025-07-30 14:23:16 +0300

    main

---
+++ lol.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+
+
+
+ctk.self_appearanse_mode("System")
+ctk.set_defaultv
+
+
+app
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from PyQt6.QtWidgets import *
 import requests
 
 
+
 app = QApplication([])
 window = QWidget()
 
 reuest1 = QLabel("Пошук")
 reuest1_2 = QLineEdit()
+searched_films= QTextEdit()
 
+popular_lbl = QLabel("Popular films:")
 
 reuest2 = QLabel("a")
 reuest3 = QLabel("x")
@@ -16,9 +19,20 @@ reuest4 = QLabel("c")
 h1 = QVBoxLayout()
 h1.addWidget(reuest1)
 h1.addWidget(reuest1_2)
+h1.addWidget(popular_lbl)
 h1.addWidget(reuest2)
 h1.addWidget(reuest3)
 h1.addWidget(reuest4)
+
+def search():
+
+    response = requests.get("https://api.themoviedb.org/3/discover/movie")
+    result = response.json()
+    response2 = requests.get("https://api.themoviedb.org/3/tv/{series_id}/keywords")
+    result2 = response2.json()
+    name = response2[popular_lbl.text()]
+    searched_films.append(name)
+    
 
 
 
