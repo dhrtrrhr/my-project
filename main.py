452034091781da82098328c27a804ee67c2b8f9f
Vodtam657 <dmitrooo333@gmail.com>
2025-08-13 14:30:33 +0300

    main

--- main.py
+++ main.py
@@ -11,27 +11,45 @@ reqest1 = QPushButton("Пошук")
 reqest1_2 = QLineEdit()
 reqest1_3 = QLabel("Popular Films:")
 
+reqest1_4 = QTextEdit()
+
 reuest2 = QLabel("a")
 reuest3 = QLabel("x")
 reuest4 = QLabel("c")
 
-h1 = QVBoxLayout()
-h1.addWidget(reqest1)
-h1.addWidget(reqest1_2)
-h1.addWidget(reqest1_3)
-h1.addWidget(reuest2)
-h1.addWidget(reuest3)
-h1.addWidget(reuest4)
+
+
 
 def search():
    
     response = requests.get(f"https://api.themoviedb.org/3/search/movie?query={reqest1_2.text()}", headers=headers)
     result = response.json()
     print(result)
-    response2 = requests.get("https://api.themoviedb.org/3/movie/{movie_id}/images")
-    result2 = response.json()
-    name = result[reqest1_2.text()]
-    # poster = response2[name]
+
+    response3 = requests.get("https://api.themoviedb.org/3/movie/popular", headers = headers)
+    result3 = response3.json()
+    print(result3)
+
+
+
+    result = result["results"]
+    film_id = result[0]["id"]
+    response2 = requests.get(f"https://api.themoviedb.org/3/movie/{film_id}/images", headers = headers)
+    result2 = response2.json()
+    poster = result2["backdrops"][0]["file_path"]
+    print(film_id)
+    print(response2.json())
+    reuest2.setText(poster)
+
+
+h1 = QVBoxLayout()
+h1.addWidget(reqest1)
+h1.addWidget(reqest1_2)
+h1.addWidget(reuest2)
+h1.addWidget(reqest1_3)
+
+h1.addWidget(reuest3)
+h1.addWidget(reuest4)
 reqest1.clicked.connect(search)
 
 
